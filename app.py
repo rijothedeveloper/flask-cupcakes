d90238c56a0d16dcf9059eb7471f0a9b8bf319db
Rijo George <rijothedeveloper@gmail.com>
2021-12-27 18:49:50 -0800

    Part One: Cupcake Model

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
+from flask import Flask
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "oh-so-secret"
+debug = DebugToolbarExtension(app)
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
 """Flask app for Cupcakes"""
